d09a3d37359c9414e19622e1cabd1716da2f9ddd
coder1311 <1311ritu.raj@gmail.com>
2021-07-08 12:56:31 +0530

    Initial commit

---
+++ build/precache-manifest.c11b8e3321179a8a0ee8b58b9546155b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c655bfea1ffe97f50822b124584e50",
+    "url": "/Chit-Chat-Room/index.html"
+  },
+  {
+    "revision": "fa9371fe0af7cd5d9ede",
+    "url": "/Chit-Chat-Room/static/css/main.346e8421.chunk.css"
+  },
+  {
+    "revision": "53fbb3853bda0a4099ef",
+    "url": "/Chit-Chat-Room/static/js/2.1c0e7173.chunk.js"
+  },
+  {
+    "revision": "fa9371fe0af7cd5d9ede",
+    "url": "/Chit-Chat-Room/static/js/main.45e29c8b.chunk.js"
+  },
+  {
+    "revision": "5c5cb16462c9cebe7e9f",
+    "url": "/Chit-Chat-Room/static/js/runtime~main.b52a4537.js"
+  }
+]);
